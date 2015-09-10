d561df469875457c3f18478e6e3f8f1c32a46995
johnballantyne <johnballantyne@gmail.com>
2015-09-10 05:58:54 -0400

    Here's some working code
    Why would you commit working code?

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-var fs = require('fs');
-var path = require('path');
 var youtubedl = require('youtube-dl');
 var config = require('./config.json');
 
 var videoURL = config.videoURL,
     video = youtubedl(videoURL);
 
-video.on('info', function(info) {
-    var output;
-
-    console.log('Download started');
-    console.log(JSON.stringify(info));
-    output = path.join(__dirname, 'out.mp4');
-    video.pipe(fs.createWriteStream(output));
-});
-
-video.on('error', function(error) {
-    console.log(error);
+youtubedl.exec(videoURL, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
+      if (err) throw err;
+        console.log(output.join('\n'));
 });
 
