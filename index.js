9e582ec5ccb3b1a41a890112fef4660cc5d82283
John <johnballantyne@gmail.com>
2015-09-13 23:33:43 -0400

    Add scheduled task
    I have no idea if this would work properly.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var youtubedl = require('youtube-dl');
+var schedule = require('node-schedule');
 var config = require('./config.json');
 
-var videoURL = config.videoURL,
-    video = youtubedl(videoURL);
+var videoURL = config.videoURL;
 
-youtubedl.exec(videoURL, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
+var getStream = youtubedl.exec(videoURL, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
       if (err) throw err;
         console.log(output.join('\n'));
 });
+
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = new schedule.Range(1, 5);
+rule.hour = 4;
+rule.minute = 30;
+
+var j = schedule.scheduleJob(rule, getStream);
 
