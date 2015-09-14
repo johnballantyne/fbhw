909141d4afba52c9ae58955580b48879c4d9ee94
johnballantyne <johnballantyne@gmail.com>
2015-09-14 00:17:49 -0400

    Potentially working scheduled download
    But I'm not entirely sure. We'll see.

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ var config = require('./config.json');
 
 var videoURL = config.videoURL;
 
-var getStream = youtubedl.exec(videoURL, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
-      if (err) throw err;
-        console.log(output.join('\n'));
-});
+function getStream() {
+    youtubedl.exec(videoURL, [], {},
+        function(err, output) {
+            if (err) {
+                console.log("Error thrown");
+                console.log(err);
+                return;
+            }
+            console.log(output.join('\n'));
+        }
+    ); 
+}
 
+// M-F, 4:00 - 5:59
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = new schedule.Range(1, 5);
-rule.hour = 4;
-rule.minute = 30;
+rule.hour = new schedule.Range(4, 5);
+rule.minute = new schedule.Range(0, 59);
+rule.second = 0;
 
-var j = schedule.scheduleJob(rule, getStream);
+var j = schedule.scheduleJob(rule, function () {
+    console.log('Attempting download...');
+    getStream();
+});
 
