8fdf3ff8306a1cd8f891c8252961c15c585597c6
johnballantyne <johnballantyne@gmail.com>
2015-09-15 22:23:02 -0400

    Switch to rtmpdump instead of YouTube
    Still unsure if this will actually work

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-var youtubedl = require('youtube-dl');
+var fs = require('fs');
+var rtmpdump = require('rtmpdump');
 var schedule = require('node-schedule');
 var config = require('./config.json');
 
-var videoURL = config.videoURL;
 
 function getStream() {
-    youtubedl.exec(videoURL, [], {},
-        function(err, output) {
-            if (err) {
-                console.log("Error thrown");
-                console.log(err);
-                return;
-            }
-            console.log(output.join('\n'));
-        }
-    ); 
+    console.log(new Date().toISOString());
+    console.log('Attempting download...');
+    
+    var videoURL = config.videoURL,
+        options = {
+            rtmp: videoURL,
+            stop: 120,
+            quiet: null,
+            live: null
+        },
+        stream = rtmpdump.createStream(options);
+
+    stream.on('connected', function (info) {
+        console.log(info);
+    });
+
+    stream.on('progress', function (kBytes, elapsed) {
+        console.log('%s kBytes read, %s secs elapsed', kBytes, elapsed);
+    });
+
+    stream.on('error', function (err) {
+        console.log(err);
+    });
+
+    var dateISO = new Date().toISOString();
+    dateISO = dateISO.split('.')[0].replace(/-|:/g, '').replace(/T/, '-');
+    stream.pipe(fs.createWriteStream(dateISO));
 }
 
 // M-F, 4:00 - 5:59
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = new schedule.Range(1, 5);
-rule.hour = new schedule.Range(4, 5);
-rule.minute = new schedule.Range(0, 59);
+rule.hour = new schedule.Range(4, 10);
+rule.minute = [5, 20, 35, 50];
 rule.second = 0;
 
-var j = schedule.scheduleJob(rule, function () {
-    console.log('Attempting download...');
-    getStream();
-});
+var j = schedule.scheduleJob(rule, getStream); 
+
+console.log(new Date().toString());
+console.log("Script has started.");
 
